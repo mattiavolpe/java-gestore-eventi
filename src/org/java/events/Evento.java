5f4d51213e563de67d742ba2f7807ca7b3b97df9
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-09-25 11:35:29 +0200

    ADD milestone 2

--- src/org/java/events/Evento.java
+++ src/org/java/events/Evento.java
@@ -28,14 +28,12 @@ public class Evento {
 	}
 	
 	public void setDate(String date) throws Exception {
-		short[] dateContainer = new short[3];
-		dateContainer[0] = Short.valueOf(date.substring(0, date.indexOf("-")));
-		dateContainer[1] = Short.valueOf(date.substring(date.indexOf("-") + 1, date.lastIndexOf("-")));
-		dateContainer[2] = Short.valueOf(date.substring(date.lastIndexOf("-") + 1));
-		this.date = LocalDate.of(dateContainer[2], dateContainer[1], dateContainer[0]);
+		LocalDate tempDate = convertInputDate(date);
 		
-		if (getDate().isBefore(LocalDate.now()))
+		if (tempDate.isBefore(LocalDate.now()))
 			throw new Exception("The date can't be in the past");
+		
+		this.date = tempDate;
 	}
 	
 	public int getTotalPlaces() {
@@ -57,16 +55,47 @@ public class Evento {
 		this.reservedPlaces = reservedPlaces;
 	}
 	
-	public void reserve() {
-		setReservedPlaces(getReservedPlaces() + 1);
+	public void reserve(int places) throws Exception {		
+		if (getTotalPlaces() < (getReservedPlaces() + places))
+			throw new Exception(
+					getTotalPlaces() - getReservedPlaces() == 0
+					? "There are no more available places"
+					: "Still available places: " + (getTotalPlaces() - getReservedPlaces())
+			);
+		
+		setReservedPlaces(getReservedPlaces() + places);
 	}
 	
-	public void remove() {
-		setReservedPlaces(getReservedPlaces() - 1);
+	public void remove(int places) {
+		
+		
+		setReservedPlaces(getReservedPlaces() - places);
+	}
+	
+	private LocalDate convertInputDate(String date) throws Exception {
+		short[] dateContainer = new short[3];
+		
+//		USING try / catch TO ALSO CATCH ERRORS ON Short.valueOf STATEMENTS
+		try {
+			dateContainer[0] = Short.valueOf(date.substring(0, date.indexOf("-")));
+			dateContainer[1] = Short.valueOf(date.substring(date.indexOf("-") + 1, date.lastIndexOf("-")));
+			dateContainer[2] = Short.valueOf(date.substring(date.lastIndexOf("-") + 1));			
+		} catch (Exception e) {
+			throw new Exception("Insert a valid date");
+		}
+		
+		return LocalDate.of(dateContainer[2], dateContainer[1], dateContainer[0]);
+	}
+	
+	private String formatDateForOutput() {
+		int day = date.getDayOfMonth();
+		int month = date.getMonthValue();
+		int year = date.getYear();
+		return String.format("%02d", day) + "-" + String.format("%02d", month) + "-" + String.format("%04d", year);
 	}
 	
 	@Override
 	public String toString() {
-		return getDate() + " - " + getTitle();
+		return formatDateForOutput() + " - " + getTitle();
 	}
 }
---
+++ src/org/java/events/Main.java
@@ -0,0 +1,90 @@
+package org.java.events;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Evento evento = null;
+		
+		System.out.print("Enter the event's title: ");
+		String title = sc.nextLine();
+		
+		System.out.print("\nEnter the event's date in the format 'dd-mm-yyyy': ");
+		String date = sc.nextLine();
+		
+		try {
+			System.out.print("\nEnter the event's total places number: ");
+			int totalPlaces = Integer.valueOf(sc.nextLine());
+		
+			evento = new Evento(title, date, totalPlaces);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		System.out.println("\nYou just created the following event: \n----------\n" + evento + "\n----------");
+		
+		int placesToReserve = 0;
+		int reservedPlaces = 0;
+		
+		do {
+			try {
+				System.out.print("\nEnter the number of places you want to RESERVE (enter 0 if you don't want to make a reservation): ");
+				placesToReserve = Integer.valueOf(sc.nextLine());
+				
+				if (placesToReserve < 0)
+					System.err.println("You can't reserve less than 1 place");
+				else if (placesToReserve > 0) {
+					evento.reserve(placesToReserve);
+					reservedPlaces += placesToReserve;
+					
+					System.err.println("\nYou reserved a total of " + reservedPlaces + " places");
+					System.err.println("Still " + (evento.getTotalPlaces() - evento.getReservedPlaces()) + " places available");
+				}
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		} while (placesToReserve != 0 && (evento.getTotalPlaces() - evento.getReservedPlaces() > 0));
+		
+		if (placesToReserve != 0)
+			System.err.println("\nNo more available places to reserve");
+		else
+			System.err.println("\nYou reserved a total of " + reservedPlaces + " places");
+		
+		int placesToRemove = 0;
+		int removedPlaces = 0;
+		
+		do {
+			if (reservedPlaces > 0) {
+				try {
+					System.out.print("\nEnter the number of places you want to REMOVE (enter 0 if you don't want to remove any place): ");
+					placesToRemove = Integer.valueOf(sc.nextLine());
+					
+					if (placesToRemove < 0)
+						System.err.println("You can't remove less than 1 place");
+					else if (placesToRemove > 0 && reservedPlaces >= placesToRemove) {
+						evento.remove(placesToRemove);
+						removedPlaces += placesToRemove;
+						reservedPlaces -= placesToRemove;
+						
+						System.err.println("\nYou successfully removed " + placesToRemove + " places");
+						System.err.println("\nYou still have a total of " + reservedPlaces + " places");
+						System.err.println("Still " + (evento.getTotalPlaces() - evento.getReservedPlaces()) + " places available");
+					} else if (placesToRemove > 0){
+						System.err.println("\nYou tried to remove " + placesToRemove + " places, but you only have " + reservedPlaces + " places still reserved");
+					}
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		} while (placesToRemove != 0 && reservedPlaces > 0);
+		
+		if (placesToRemove != 0)
+			System.err.println("\nNo more available places");
+		else
+			System.err.println("\nYou reserved a total of " + reservedPlaces + " places");
+		
+		sc.close();
+	}
+}
